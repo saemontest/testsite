1a597b350e57e584d8bd1cb232e9b12e35bc4d94
Saemon <pythonusertest@gmail.com>
2019-05-12 22:59:49 +0300

    создали index и about и вьюхи к ним

--- testsite/settings.py
+++ testsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'testsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]# закрепляем папку temlates
         ,
         'APP_DIRS': True,
         'OPTIONS': {
