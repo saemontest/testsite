9bc942cf09480f579b5c9ff9c4f3ee5e618dde29
Saemon <pythonusertest@gmail.com>
2019-05-13 20:12:46 +0300

    новый шаблон

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def about(request):
-    return render(request, 'about.html')
+def blog(request):
+    return render(request, 'blog.html')
